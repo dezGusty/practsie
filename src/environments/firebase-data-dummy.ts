5e2b34ebb1ae8bc7e6a60b056974e7f95c8a205d
Augustin Preda <thegusty999@gmail.com>
2021-02-26 16:02:49 +0200

    add dummy firebase config file

---
+++ src/environments/firebase-data-dummy.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "<blabla-key>",
+  authDomain: "<blabla-domain>",
+  databaseURL: "https://<blabla>.firebaseio.com",
+  projectId: "<bla>",
+  storageBucket: "<blabla>.appspot.com",
+  messagingSenderId: "<blablaid>",
+  appId: "<blablaotherid>",
+  measurementId: "<blablablab>"
+};
